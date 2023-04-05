cabeabfe6973ae932fca24a3021089d02854e46d
muhammadqamar <muhammadqamar111@gmail.com>
2023-04-05 18:09:45 +0500

    design update

--- src/C2EComponents/myc2e.js
+++ src/C2EComponents/myc2e.js
@@ -1,23 +1,21 @@
-import React, { useState, useRef } from 'react';
-import c2e from '../assets/images/c2e.png';
+import React, { useState, useEffect } from 'react';
 
 import { Web3Auth } from '@web3auth/modal';
 import { ADAPTER_EVENTS } from '@web3auth/base';
+import { OpenloginAdapter } from '@web3auth/openlogin-adapter';
+// import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
 import upload from '../assets/images/upload (1).svg';
-import JSZip from 'jszip';
 import Myc2eOverview from './myc2eoverview';
 import Header from './header';
-import { useEffect } from 'react';
+
+import UploadFile from './upload';
+
 const Myc2e = () => {
-  const [contentData, setcontentData] = useState();
-  const [contentDetail, setcontentDetail] = useState(null);
-  const [projectJSON, setProjectJSON] = useState(null);
-  const [playlists, setplaylists] = useState([]);
-  const [activityh5p, setActivityh5p] = useState(null);
-  const [loadzipper, setloadzipper] = useState(null);
+  const [uploadProgress, setUploadProgress] = useState(0);
   const [web3auth, setWeb3auth] = useState(null);
   const [walletConnection, setWalletConneciton] = useState(null);
-  const inp = useRef();
 
   const login = async () => {
     if (!web3auth) {
@@ -69,137 +67,65 @@ const Myc2e = () => {
   return (
     <div className="reader-c2e">
       <Header web3auth={web3auth} walletConnection={walletConnection} />
-      {!contentDetail ? (
-        <div className="reader-main">
-          {/*<div className="img-box">
+
+      <div className="reader-main">
+        {/*<div className="img-box">
             <img src={c2e} alt="logo" />
            </div>*/}
 
-          {walletConnection ? (
-            <div className="login-text text-detail">
-              <h3>How does it work?</h3>
-              <p>
-                After you have licensed a C2E from a digital marketplace, you will receive an email
-                with instructions on how to download it.
-                <br /> After the C2E is in your hand, you can open it in a C2E reader, like this
-                one.
-              </p>
-            </div>
-          ) : (
-            <div className="text-detail">
-              <h5>Imagine a world…</h5>
-              <p>
-                … where access to high quality learning resources is equitable, affordable, and
-                widely available. <br />
-                <br /> … where digital content creators have access to resources and incentives to
-                build quality interactive learning experiences, and marketplaces where they can get
-                paid fairly for their efforts.
-                <br /> <br /> … where the quality and appropriateness of digital educational content
-                can be vetted before it enters a marketplace.
-              </p>
-            </div>
-          )}
+        {walletConnection ? (
+          <div className="login-text text-detail">
+            <h3>How does it work?</h3>
+            <p>
+              After you have licensed a C2E from a digital marketplace, you will receive an email
+              with instructions on how to download it.
+              <br /> After the C2E is in your hand, you can open it in a C2E reader, like this one.
+            </p>
+          </div>
+        ) : (
+          <div className="text-detail">
+            <h5>Imagine a world…</h5>
+            <p>
+              … where access to high quality learning resources is equitable, affordable, and widely
+              available. <br />
+              <br /> … where digital content creators have access to resources and incentives to
+              build quality interactive learning experiences, and marketplaces where they can get
+              paid fairly for their efforts.
+              <br /> <br /> … where the quality and appropriateness of digital educational content
+              can be vetted before it enters a marketplace.
+            </p>
+          </div>
+        )}
 
-          <div className="uploadBox">
-            <div className="box">
-              <h1>Curriki Educational Experiences Writer</h1>
-              {walletConnection && (
-                <div
-                  className="iconbox"
-                  onClick={() => {
-                    inp.current.click();
-                  }}
-                >
-                  <img src={upload} alt="logo" />
+        <div className="uploadBox">
+          <div className="box">
+            <h1>Curriki Educational Experiences Writer</h1>
+            {walletConnection && (
+              <>
+                <div className="iconbox">
+                  <img src={upload} alt="doge" />
                 </div>
-              )}
-              {walletConnection ? (
-                <p className="text">Upload C2E from your local device</p>
-              ) : (
-                <p className="text text-space">Log In and Experience C2Es NOW</p>
-              )}
-              {walletConnection ? (
-                <button
-                  onClick={() => {
-                    inp.current.click();
-                  }}
-                >
-                  Choose File
-                </button>
-              ) : (
-                <button onClick={() => login()}>LET’s GET STARTED!</button>
-              )}
-              <input
-                ref={inp}
-                type="file"
-                style={{ display: 'none' }}
-                onChange={async (e) => {
-                  console.log('name', e.target.files);
-                  const loadzip = await JSZip.loadAsync(e.target.files[0]); //  read the zip file
-                  if (!loadzip.files['c2e.json']) {
-                    alert('please uplaod only C2E Project, c2e.json not found.');
-                    return false;
-                  } else {
-                    // set state for all zip files
-                    setloadzipper(loadzip);
-                  }
+                <ProgressBar now={uploadProgress} label={`${uploadProgress}%`} visuallyHidden />
+              </>
+            )}
 
-                  const contents = [];
-                  const contentsDetail = [];
-                  loadzip.forEach((relativePath, zipEntry) => {
-                    // load inner content for each file
-                    contents.push(zipEntry.name);
-                  });
-                  setcontentData(contents);
-                  // iterate each file through loop
-                  for (var i = 0; i < contents.length; i++) {
-                    const contentRead = await loadzip.files[contents[i]].async('text');
-                    contentsDetail.push(contentRead);
-
-                    if (contents[i].includes('c2e.json') && !contents[i].includes('activities')) {
-                      const c2edata = JSON.parse(contentRead);
-                      c2edata?.c2eContain?.[0].c2eResources?.map(async (resource) => {
-                        // set project json after filter
-                        if (resource.url === 'project.json') {
-                          const contentProject = await loadzip.files['project.json'].async('text');
-                          setProjectJSON(JSON.parse(contentProject));
-                        }
-                        // extract array  of playlist depend on directory present in resoruce folder
-                        if (
-                          resource.fileFormat === 'directory' &&
-                          resource.url !== 'activities' &&
-                          resource.url !== 'playlists'
-                        ) {
-                          setplaylists((prevplaylists) => [...prevplaylists, resource.url]);
-                        }
-                      });
-                    }
-                  }
-                  setcontentDetail(contentsDetail);
-                }}
-              />
-            </div>
+            {walletConnection ? (
+              <p className="text">Upload C2E from your local device</p>
+            ) : (
+              <p className="text text-space">Log In and Experience C2Es NOW</p>
+            )}
+            {walletConnection ? (
+              <UploadFile setUploadProgress={setUploadProgress} />
+            ) : (
+              <button onClick={() => login()}>LET’s GET STARTED!</button>
+            )}
           </div>
         </div>
-      ) : (
-        <div className="playlist-informtion">
-          {/* <button
-            onClick={() => {
-              setcontentDetail();
-            }}
-          >
-            Back
-          </button>*/}
-          <Myc2eOverview
-            playlistsContent={playlists}
-            setActivityh5p={setActivityh5p}
-            contentDetail={contentDetail}
-            contents={contentData}
-            loadzipper={loadzipper}
-            activityh5p={activityh5p}
-          />
-        </div>
-      )}
+      </div>
+      {/* <div className="iconbox">
+        <img src={upload} alt="logo" />
+      </div>
+            {uploadProgress > 0 && <div>Progress: {uploadProgress}%</div>}*/}
       {/*<footer class="footer-all">
         <a
           rel="noreferrer"
--- src/C2EComponents/upload.js
+++ src/C2EComponents/upload.js
@@ -1,31 +1,23 @@
 import React, { useState, useRef } from 'react';
 import axios from 'axios';
 
-function UploadFile() {
-  const [uploadProgress, setUploadProgress] = useState(0);
-
+function UploadFile({ setUploadProgress }) {
   const fileinput = useRef();
   const handleUpload = async (event) => {
     const formData = new FormData();
-    console.log(event.target.files[0])
+    console.log(event.target.files[0]);
     formData.append('uploadFile', event.target.files[0]);
 
     try {
-      const response = await axios.post(
-        'https://writer-dev.curriki.org/upload',
-        formData,
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-          onUploadProgress: (progressEvent) => {
-            const percentCompleted = Math.round(
-              (progressEvent.loaded * 100) / progressEvent.total
-            );
-            setUploadProgress(percentCompleted);
-          },
-        }
-      );
+      const response = await axios.post('https://writer-dev.curriki.org/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        onUploadProgress: (progressEvent) => {
+          const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+          setUploadProgress(percentCompleted);
+        },
+      });
       // Download the file in the response
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement('a');
@@ -55,7 +47,6 @@ function UploadFile() {
       >
         Choose File
       </button>
-      {uploadProgress > 0 && <div>Progress: {uploadProgress}%</div>}
     </div>
   );
 }
