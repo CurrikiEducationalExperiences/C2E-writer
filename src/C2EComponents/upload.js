aae9114710a08d33a1eb51af5666a2e1c8f3cef6
muhammadqamar <muhammadqamar111@gmail.com>
2023-04-05 15:03:21 +0500

    added downloaderr

--- src/C2EComponents/upload.js
+++ src/C2EComponents/upload.js
@@ -2,10 +2,7 @@ import React, { useState, useRef } from 'react';
 import axios from 'axios';
 
 function UploadFile() {
-
   const [uploadProgress, setUploadProgress] = useState(0);
-
-
 
   const fileinput = useRef();
   const handleUpload = async (event) => {
@@ -28,8 +25,13 @@ function UploadFile() {
           },
         }
       );
-
-      console.log(response.data);
+      // Download the file in the response
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'file.pdf');
+      document.body.appendChild(link);
+      link.click();
     } catch (error) {
       console.error(error);
     }
@@ -40,7 +42,6 @@ function UploadFile() {
       <input
         style={{ display: 'none' }}
         onChange={(event) => {
-
           handleUpload(event);
         }}
         ref={fileinput}
