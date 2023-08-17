aa05c7dd322afcc195d70ff86e9966a20deb2f8f
muhammadqamar <muhammadqamar111@gmail.com>
2023-08-17 14:52:43 +0500

    more changes

--- src/C2EComponents/upload.js
+++ src/C2EComponents/upload.js
@@ -1,55 +1,97 @@
 import React, { useState, useRef } from 'react';
 import axios from 'axios';
 import fileDownload from 'js-file-download';
-import loader from '../assets/images/loader.png';
+import h5p from '../assets/images/assig2.png';
+import epub from '../assets/images/assig1.png';
+import Modal from 'react-bootstrap/Modal';
 import { Spinner } from 'react-bootstrap';
+import { Formik } from 'formik';
 function UploadFile({ setUploadProgress }) {
   const fileinput = useRef();
   const [startConversion, setStartConversion] = useState(false);
+  const [show, setShow] = useState(false);
+  const [epubmeta, setEpubData] = useState();
   const handleUpload = async (event) => {
     const formData = new FormData();
-    console.log(event.target.files[0]);
-    formData.append('uploadFile', event.target.files[0]);
 
-    try {
-      const response = await axios.post(
-        'https://writer-dev.curriki.org/upload',
-        formData,
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-          onUploadProgress: (progressEvent) => {
-            const percentCompleted = Math.round(
-              (progressEvent.loaded * 100) / progressEvent.total
-            );
-            setUploadProgress(percentCompleted);
-            console.log(percentCompleted);
-            if (percentCompleted === 100) {
-              setStartConversion(true);
-            }
-          },
-          responseType: 'blob',
-        }
-      );
-      setStartConversion(false);
-      // Download the file in the response
-      //fileDownload(response.data,`${event.target.files[0].name.split('.')[0]}.zip`)
-      fileDownload(
-        response.data,
-        event.target.files[0].name.replace('.zip', '.c2e')
-      );
-      // const url = window.URL.createObjectURL(new Blob([response.data],{ type: 'application/octet-stream' }));
-      // const link = document.createElement('a');
-      // link.href = url;
-      // link.setAttribute('download', `${event.target.files[0].name.split('.')[0]}.zip`);
-      // document.body.appendChild(link);
-      // link.click();
-    } catch (error) {
-      console.error(error);
+    formData.append('uploadFile', event.target.files[0]);
+    if (epubmeta) {
+
+        const response = await axios.post(
+          'https://c2e-provider-api.curriki.org/c2e-media/upload',
+          formData,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+            onUploadProgress: (progressEvent) => {
+              const percentCompleted = Math.round(
+                (progressEvent.loaded * 100) / progressEvent.total
+              );
+              setUploadProgress(percentCompleted);
+              console.log(percentCompleted);
+              if (percentCompleted === 100) {
+                setStartConversion(true);
+              }
+            },
+            responseType: 'blob',
+          }
+
+        );
+        setStartConversion(false);
+        // Download the file in the response
+        //fileDownload(response.data,`${event.target.files[0].name.split('.')[0]}.zip`)
+        // fileDownload(
+        //   response.data,
+        //   event.target.files[0].name.replace('.zip', '.c2e')
+        // );
+    } else {
+      try {
+        const response = await axios.post(
+          'https://writer-dev.curriki.org/upload',
+          formData,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+            onUploadProgress: (progressEvent) => {
+              const percentCompleted = Math.round(
+                (progressEvent.loaded * 100) / progressEvent.total
+              );
+              setUploadProgress(percentCompleted);
+              console.log(percentCompleted);
+              if (percentCompleted === 100) {
+                setStartConversion(true);
+              }
+            },
+            responseType: 'blob',
+          }
+        );
+        setStartConversion(false);
+        // Download the file in the response
+        //fileDownload(response.data,`${event.target.files[0].name.split('.')[0]}.zip`)
+        fileDownload(
+          response.data,
+          event.target.files[0].name.replace('.zip', '.c2e')
+        );
+        // const url = window.URL.createObjectURL(new Blob([response.data],{ type: 'application/octet-stream' }));
+        // const link = document.createElement('a');
+        // link.href = url;
+        // link.setAttribute('download', `${event.target.files[0].name.split('.')[0]}.zip`);
+        // document.body.appendChild(link);
+        // link.click();
+      } catch (error) {
+        console.error(error);
+      }
     }
   };
-
+  const selectSelection = (type) => {
+    if (type === 'h5p') {
+      fileinput.current.click();
+    } else {
+      setEpubData(true);
+    }
+  };
   return (
     <>
       <div>
@@ -63,7 +105,8 @@ function UploadFile({ setUploadProgress }) {
         />
         <button
           onClick={() => {
-            fileinput.current.click();
+            //fileinput.current.click();
+            setShow(true);
           }}
         >
           Choose File
@@ -80,6 +123,147 @@ function UploadFile({ setUploadProgress }) {
           </div>
         </div>
       )}
+      <Modal
+        show={show}
+        onHide={() => {
+          setShow(false);
+        }}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Body>
+          {epubmeta ? (
+            <Formik
+              initialValues={{
+                title: '',
+                description: '',
+                name: '',
+                email: '',
+                url: '',
+              }}
+              validate={(values) => {
+                const errors = {};
+                if (!values.title) {
+                  errors.title = 'Required';
+                }
+                if (!values.description) {
+                  errors.description = 'Required';
+                }
+                if (!values.name) {
+                  errors.name = 'Required';
+                }
+                if (!values.email) {
+                  errors.email = 'Required';
+                } else if (
+                  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                ) {
+                  errors.email = 'Invalid email address';
+                }
+                return errors;
+              }}
+              onSubmit={(values, { setSubmitting }) => {
+                fileinput.current.click();
+
+              }}
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+                /* and other goodies */
+              }) => (
+                <form onSubmit={handleSubmit}>
+                  <div class="form-group">
+                    <label for="title">Title:</label>
+                    <input
+                      name="title"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.title}
+                    />
+                    {errors.title && touched.title && errors.title}
+                  </div>
+                  <div class="form-group">
+                    <label for="description">Description:</label>
+                    <textarea
+                      name="description"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.description}
+                    ></textarea>
+                    {errors.description &&
+                      touched.description &&
+                      errors.description}
+                  </div>
+
+                  <h2>C2E Licensee Information</h2>
+
+                  <div class="form-group">
+                    <label for="licensee_name">Name:</label>
+                    <input
+                      name="name"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.name}
+                      type="text"
+                    />
+                    {errors.name && touched.name && errors.name}
+                  </div>
+                  <div class="form-group">
+                    <label for="licensee_email">Email:</label>
+                    <input
+                      name="email"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email}
+                      type="email"
+                    />
+                    {errors.email && touched.email && errors.email}
+                  </div>
+                  <div class="form-group">
+                    <label for="licensee_url">URL:</label>
+                    <input
+                      name="url"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.url}
+                      type="url"
+                    />
+                  </div>
+
+                  <button type="submit" class="btn btn-primary">
+                    Upload
+                  </button>
+                </form>
+              )}
+            </Formik>
+          ) : (
+            <div className="selection-box">
+              <div
+                className="box"
+                onClick={() => {
+                  selectSelection('h5p');
+                }}
+              >
+                <img src={h5p} alt="" />
+              </div>
+              <div
+                className="box"
+                onClick={() => {
+                  selectSelection('epub');
+                }}
+              >
+                <img src={epub} alt="" />
+              </div>
+            </div>
+          )}
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
