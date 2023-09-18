5d6bd7511f4d6be6d930195c7fad304a4f9fba59
Antonio Etayo <antonioetayo@gmail.com>
2023-09-18 08:26:23 -0400

    Changing auth libraries

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { createContext } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import jwt_decode from "jwt-decode";
 import Myc2e from './C2EComponents/myc2e';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+export const UserContext = createContext(null);
+
 function App() {
+  const OAuthClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+  const token = localStorage.getItem('oAuthToken');
+  const user = token ? jwt_decode(token) : null;
+  
   return (
-    <Router>
-      <div className="ts-particles"></div>
-      <Switch>
-        <Route exact path="/">
-          <Myc2e />
-        </Route>
-      </Switch>
-    </Router>
+    <GoogleOAuthProvider clientId={OAuthClientId}>
+      <UserContext.Provider value={user}>
+        <Router>
+          <div className="ts-particles"></div>
+          <Switch>
+            <Route exact path="/">
+              <Myc2e />
+            </Route>
+          </Switch>
+        </Router>
+      </UserContext.Provider>
+    </GoogleOAuthProvider>
   );
 }
 
